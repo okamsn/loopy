8d523cae3986a4f6a1b191f58ceffb883ff4168f
okamsn <okamsn@users.noreply.github.com>
2021-02-16 23:24:40 -0500

    Update the package description.
    Copy the intro of the Info document into the package description.

--- loopy.el
+++ loopy.el
@@ -31,9 +31,40 @@
 ;; commands" that define a loop body and it's surrounding environment, as well
 ;; as exit conditions.
 ;;
-;; For more information, see Loopy’s README.org or the Info files derived from
-;; that README with the command `info'.
-
+;; The `loopy' macro has several possible arguments, each beginning with a
+;; keyword.
+;;
+;; - `with' declares variables that are bound in order before and around the
+;; loop, like in a `let*' binding.
+;; - `without' declares variables that ~loopy~ should not try to initialize.
+;; - `before-do' is a list of normal Lisp expressions to run before the loop
+;; executes.
+;; - `loop' is a list of special commands that create the loop body.  These
+;; commands are described in detail in this package's Info document.
+;; - `after-do' is a list of normal Lisp expressions to run after the successful
+;; completion of the loop.
+;; - `finally-do' is a list of normal Lisp expressions that always run,
+;; regardless of whether an early return was triggered in the loop body.
+;; - `finally-return' is an expression whose value is always returned, regardless
+;; of whether an early return was triggered in the loop body.
+;; - `flags' is a list of symbols that change the macro's behavior.
+;;
+;; The loop commands generally follow the form `(COMMAND VARIABLE-NAME &rest ARGS)'.
+;; For example,
+;;
+;; - To iterate through a sequence, use `(seq elem [1 2 3])' (for
+;;   efficiency, there are also more specific commands, like `list').
+;; - To collect values into a list, use `(collect my-collection collected-value)'.
+;; - To just bind a variable to the result of a Lisp expression, use
+;;   `(expr my-var (my-func))'
+;;
+;; Below is a full example of the arguments of the `loopy' macro.  The top-level
+;; forms have a flexible-order, but meaning is clearest if they have the
+;; following order.  All of the arguments are technically optional, but having a
+;; loop without a body wouldn't be useful.
+;;
+;; For more information, see this package's Info documentation under Info node
+;; `(loopy)'.
 
 ;;; Code:
 
