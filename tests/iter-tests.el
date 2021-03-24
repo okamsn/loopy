b26f775f677600b88b3a7295785e73bb6e73e169
okamsn <okamsn@users.noreply.github.com>
2021-03-24 19:38:02 -0400

    Quote macro correctly in tests.

--- tests/iter-tests.el
+++ tests/iter-tests.el
@@ -9,14 +9,14 @@
 
 (ert-deftest just-commands ()
   (should (equal '(1 2 3)
-                 (loopy-iter (for list i '(1 2 3))
-                             (accum collect i)))))
+                 (eval (quote (loopy-iter (for list i '(1 2 3))
+                                          (accum collect i)))))))
 
 (ert-deftest wrap-in-let ()
   (should (equal '(2 4 6)
-                 (loopy-iter (for list i '(1 2 3))
-                             (let ((a i))
-                               (accum collect (+ a i)))))))
+                 (eval (quote (loopy-iter (for list i '(1 2 3))
+                                          (let ((a i))
+                                            (accum collect (+ a i)))))))))
 
 (ert-deftest dont-swap-let-var-args ()
   "Parse `let'-like forms correctly.
@@ -73,35 +73,43 @@ E.g., \"(let ((for list)) ...)\" should not try to operate on the
 (ert-deftest wrap-lambda ()
   "Both quoted an unquoted lambda's should be wrapped."
   (should (equal '(1 2 3)
-                 (loopy-iter (for list elem '(1 2 3))
-                             (let ((a #'(lambda (x)
-                                          (for expr j x))))
-                               (accum collect (progn
-                                                (funcall a elem)
-                                                j))))))
+                 (eval (quote (loopy-iter (for list elem '(1 2 3))
+                                          (let ((a #'(lambda (x)
+                                                       (for expr j x))))
+                                            (accum collect (progn
+                                                             (funcall a elem)
+                                                             j))))))))
   (should (equal '(1 2 3)
-                 (loopy-iter (for list elem '(1 2 3))
-                             (let ((a (lambda (x)
-                                        (for expr j x))))
-                               (accum collect (progn
-                                                (funcall a elem)
-                                                j))))))
+                 (eval (quote (loopy-iter (for list elem '(1 2 3))
+                                          (let ((a (lambda (x)
+                                                     (for expr j x))))
+                                            (accum collect (progn
+                                                             (funcall a elem)
+                                                             j))))))))
 
   ;; NOTE: `defun' expands to a use of `lambda'.
   (should (equal '(1 2 3)
-                 (loopy-iter (for list elem '(1 2 3))
-                             (let ((a (defun loopy-defun-test (x)
-                                        (for expr j x))))
-                               (accum collect (progn
-                                                (funcall a elem)
-                                                j)))))))
+                 (eval (quote (loopy-iter (for list elem '(1 2 3))
+                                          (let ((a (defun loopy-defun-test (x)
+                                                     (for expr j x))))
+                                            (accum collect (progn
+                                                             (funcall a elem)
+                                                             j)))))))))
 
 (ert-deftest wrap-while ()
   ;; `dolist' expands to `while'
   (should (equal '(1 7 2 8 3 9)
-                 (loopy-iter (for list elem '((1 7) (2 8) (3 9)))
-                             (dolist (i elem)
-                               (accum collect i))))))
+                 (eval (quote (loopy-iter (for list elem '((1 7) (2 8) (3 9)))
+                                          (dolist (i elem)
+                                            (accum collect i))))))))
 
+(ert-deftest wrap-backquote ()
+  (should (equal '((1 2) (2 4) (3 6))
+                 (eval (quote
+                        (loopy-iter (for list elem '(1 2 3))
+                                    (let ((a `(,elem ,(progn
+                                                        (for expr i (* elem 2))
+                                                        i))))
+                                      (accum collect a))))))))
 
 ;; end
