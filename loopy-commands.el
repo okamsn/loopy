3e6361d647cdbed22ff862bf6a7d53f1299eb726
okamsn <okamsn@users.noreply.github.com>
2021-07-29 23:00:55 -0400

    Fix over-eager warning in `map` regarding dotted pairs.

--- loopy-commands.el
+++ loopy-commands.el
@@ -1129,9 +1129,14 @@ extracted from a hash-map, association list, property list, or
 vector using the library `map.el'."
   (when loopy--in-sub-level
     (loopy--signal-bad-iter 'map))
-  (unless (and (consp var)
-               (not (proper-list-p var)))
-    (lwarn '(loopy) :warning "Loopy: `map' iterates through dotted pairs: %s" var))
+  ;; Previously, this command iterated through proper lists, not dotted.  This
+  ;; was changed to make it behave more like the function `map-pairs'.  We
+  ;; signal a warning when a proper list is used, since now that is usually the
+  ;; wrong behavior.
+  (when (proper-list-p var)
+    (lwarn '(loopy) :warning "Loopy: `map' iterates through dotted pairs: %s"
+           var))
+
   (let ((value-holder (gensym "map-")))
     `((loopy--iteration-vars (,value-holder (map-pairs ,val)))
       ,@(loopy--destructure-for-iteration-command var `(car ,value-holder))
