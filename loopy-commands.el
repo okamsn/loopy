da31ea700bb69e2a71cc014e3a383399ad448ad7
okamsn <okamsn@users.noreply.github.com>
2022-08-27 20:37:47 -0400

    Don't use `user-error` when expanding macros.

--- loopy-commands.el
+++ loopy-commands.el
@@ -110,10 +110,33 @@
   "Loopy: Bad command arguments"
   'loopy-error)
 
+(define-error 'loopy-bad-iter
+  "Loopy: Bad iteration command"
+  'loopy-error)
+
+(define-error 'loopy-sub-level-iter
+  "Loopy: Can't use iteration command in sub-level of a loop"
+  'loopy-bad-iter)
+
+(define-error 'loopy-no-while-iter
+  "Loopy: Can't use iteration commands in this macro"
+  'loopy-bad-iter)
+
 (defun loopy--signal-sub-level-iter (used-name true-name)
+  "Signal that iteration command COMMAND-NAME was used in a sub-level."
+  (signal
+   'loopy--signal-sub-level-iter
+   (format-message
+    "Loopy: Can only use command `%s' (`%s') in top level of a loop macro"
+    used-name true-name)))
+
+(defun loopy--signal-no-while-iter (used-name true-name)
   "Signal an error for COMMAND-NAME."
-  (user-error "Can only use command `%s' (`%s') in top level of `loopy' or sub-loop"
-              used-name true-name))
+  (signal 'loopy--signal-no-while-iter
+          (format-message
+           "Loopy: Can only use command `%s' (`%s') in the looping macros"
+           used-name true-name)))
+
 
 (defun loopy--signal-must-be-top-level (command-name)
   "Signal an error for COMMAND-NAME."
