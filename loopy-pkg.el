02a98e436cbd46b5aa61a59b78be5bc0bd0df7fa
okamsn <okamsn@users.noreply.github.com>
2024-02-25 09:05:08 -0500

    Setup branch for only `loopy-dash`.

--- loopy-pkg.el
+++
@@ -1,8 +0,0 @@
-(define-package "loopy" "0.12.0"
-  "A looping macro"
-  '((emacs "27.1")
-    (map   "3.3.1")
-    (seq   "2.22")
-    (compat "29.1.3"))
-  :homepage "https://github.com/okamsn/loopy"
-  :keywords '("extensions"))
