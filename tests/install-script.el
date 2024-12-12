54b0f67e343eddf63b12ad35ad868e4d013acb7b
okamsn <okamsn@users.noreply.github.com>
2024-12-11 20:25:53 -0500

    Try using the newer version.

--- tests/install-script.el
+++ tests/install-script.el
@@ -1,11 +1,27 @@
 (setq package-user-dir "~/.emacs.d/elpa")
 (message "Current directory: %s" default-directory)
 (require 'package)
+;; (package-install 'seq)
+;; (package-install 'map)
+;; (package-install 'stream)
+;; (package-install 'compat)
+(dolist (i '(("gnu" . "https://elpa.gnu.org/packages/")
+             ("gnu-devel" . "https://elpa.gnu.org/devel/")
+             ("nongnu" . "https://elpa.nongnu.org/nongnu/")
+             ("nongnu-devel" . "https://elpa.nongnu.org/nongnu-devel/")))
+  (add-to-list 'package-archives i))
+
+(dolist (i '(("gnu-devel" . -1)
+             ("nongnu-devel" . -1)))
+  (add-to-list 'package-archive-priorities i))
+
 (package-refresh-contents)
-(package-install 'seq)
-(package-install 'map)
-(package-install 'stream)
-(package-install 'compat)
+
+(package-install
+ (car (seq-filter (lambda (x)
+                    (equal (package-desc-archive x) "gnu-devel"))
+                  (alist-get 'stream package-archive-contents))))
+
 (message "\nInstall from dir:")
 (package-install-file "~/work/loopy/loopy/test-install")
 (message "\nInstall from file:")
