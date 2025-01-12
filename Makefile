993338c4bbd543b79370e2825163dabbef66d83e
okamsn <okamsn@users.noreply.github.com>
2025-01-11 21:17:18 -0500

    Test installing Info docs and add more Makefile targets.
    Now test installing the Info documentation using the programs `makeinfo` and
    `install-info`.
    
    For some reason, we need to copy the files manually into the installation
    directly.  `pacakge-install-buffer` will not do it, and the new Makefile target
    for tar files does not currently work.
    
    Instead of manually adding files to the load path, we activate the packages.

--- Makefile
+++ Makefile
@@ -14,3 +14,20 @@ iter-tests:
 
 misc-tests:
 	$(EMACS) -Q -batch -l ert -l tests/load-path.el -l tests/misc-tests.el -f ert-run-tests-batch-and-exit
+
+.PHONY: doc
+
+info:
+	makeinfo --verbose --output=doc/loopy.info doc/loopy.texi
+	test -f doc/loopy.info
+	install-info --debug --dir-file doc/dir --info-file doc/loopy.info
+	test -f doc/dir
+
+.PHONY: tar
+
+tar: info
+	$(eval VERSION ?= $(shell $(EMACS) -Q -batch --eval="(progn (require 'lisp-mnt) (with-temp-buffer (insert-file-contents \"loopy.el\") (princ (or (lm-header \"package-version\") (lm-header \"version\")))))"))
+	# While creating the archive, we have to switch to the 'doc' directory
+	# while processing the Info files, otherwise `tar` will include
+	# the directory structure.
+	tar --create --file "loopy-$(VERSION).tar" --exclude-from=".elpaignore" --verbose loopy*.el --directory=doc dir loopy.info
--- tests/install-script.el
+++ tests/install-script.el
@@ -1,8 +1,24 @@
+;; -*- lexical-binding: t; -*-
+
 (setq package-user-dir "~/.emacs.d/elpa")
 (message "Current directory: %s" default-directory)
 (require 'package)
 (package-refresh-contents)
+
 (message "\nInstall Loopy from dir:")
 (package-install-file (expand-file-name "loopy/" default-directory))
+
+;; `package-install-file' does not want to install these files for some reason.
+(let ((place (file-name-directory (locate-library "loopy"))))
+  (copy-file (expand-file-name "loopy/dir" default-directory)
+             (expand-file-name "dir" place))
+  (copy-file (expand-file-name "loopy/loopy.info" default-directory)
+             (expand-file-name "loopy.info" place))
+  (message "Installed Loopy to: %s" place)
+  (cl-assert (file-exists-p (expand-file-name "loopy.info" place))
+             "Info file not installed.")
+  (cl-assert (file-exists-p (expand-file-name "dir" place))
+             "Info file not installed."))
+
 (message "\nInstall Loopy Dash from file:")
 (package-install-file (expand-file-name "loopy-dash/loopy-dash.el" default-directory))
