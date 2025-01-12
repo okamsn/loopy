228781c87b245a1562ed3d5d35eaa12e97b356ac
okamsn <28612288+okamsn@users.noreply.github.com>
2025-01-12 21:23:53 +0000

    Test installing Info docs and add more Makefile targets. (#220)
    Now test installing the Info documentation using the programs `makeinfo` and
    `install-info`.
    
    For some reason, we need to copy the files manually into the installation
    directly.  `pacakge-install-buffer` will not do it, and the new Makefile target
    for tar files does not currently work.
    
    Instead of manually adding files to the load path for tests, we activate the packages like Emacs does.

--- tests/load-path.el
+++ tests/load-path.el
@@ -1,9 +1,3 @@
 ;; Add installed packages to load path.
 ;; Don't use Seq, as we want to load the right version.
-(require 'cl-lib)
-(let ((dir (expand-file-name (if (require 'package nil t)
-                                 package-user-dir
-                               "~/.emacs.d/elpa"))))
-  (cl-loop for i in (directory-files-recursively dir  "" t)
-           when (file-directory-p i)
-           do (add-to-list 'load-path i)))
+(package-activate-all)
