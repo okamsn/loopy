7bf881e30653c6c081595449eacd757e00a46bb5
Jonas Bernoulli <jonas@bernoul.li>
2025-01-15 17:31:19 +0100

    Remove outdated loopy-pkg.el

--- loopy-pkg.el
+++
@@ -1,9 +0,0 @@
-(define-package "loopy" "0.13.0"
-  "A looping macro"
-  '((emacs "27.1")
-    (map   "3.3.1")
-    (seq   "2.22")
-    (compat "29.1.3")
-    (stream "2.3.0"))
-  :homepage "https://github.com/okamsn/loopy"
-  :keywords '("extensions"))
